ed5a20ac09788e0ad1d2cecb8099713575276635
이진곤 <qazqww@naver.com>
2023-03-01 12:58:13 +0900

    Add: HelloController & index

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "spring!!");
+        return "hello"; // templates에 있는 hello를 찾아 렌더링
+    }
+}
